f9a00a14cc189f81f1538102855a2038b8563091
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-08 18:59:06 +0530

    Devise gem added and authentication is done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ActiveRecord::Base
   has_many :directions
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
-
+  belongs_to :user
   validates :title, :description, :image, presence: true
   has_attached_file :image, styles: { medium: "400x400#>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
